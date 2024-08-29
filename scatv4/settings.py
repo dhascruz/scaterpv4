fb7dbfba6406bd7d1bfd951bbdf0d689705b2121
“dhascruz” <dhascruz@gmail.com>
2024-08-29 16:09:26 +0530

    admin changes

--- scatv4/settings.py
+++ scatv4/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'scatv4.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'scatv4',
+        'NAME': 'scaterp',
         'USER': 'myadmin',
         'PASSWORD': '453cur135',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
