abc37ae1beaa12c8aafd1001c81ecc50e53a3365
“dhascruz” <dhascruz@gmail.com>
2024-08-29 16:18:40 +0530

    admin changes

--- scatv4/settings.py
+++ scatv4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-417zqa0@u=x9tz1y_l^ioqvhve(i&a!_me@xjh#ga6oku_aw_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['farmer.scatech.in',"localhost"]
 
 
 # Application definition
